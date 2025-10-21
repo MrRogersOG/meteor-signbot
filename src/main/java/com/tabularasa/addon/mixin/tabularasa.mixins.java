19d93d5e3ae1311e79ec5a281c3e93d851634f03
MrRogersOG <mrrogersmaddenfilmroom@gmail.com>
2025-10-21 15:30:00 -0500

    latest
    updates

---
+++ src/main/java/com/tabularasa/addon/mixin/tabularasa.mixins.java
@@ -0,0 +1,13 @@
+package com.tabularasa.addon.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * The main mixin entry point for the TabulaRasa addon.
+ * This file is currently empty and ready for future additions.
+ */
+@Mixin(MinecraftClient.class)
+public abstract class TabulaRasaMixin {
+    // Your future, specific mixin injections will go here.
+}
